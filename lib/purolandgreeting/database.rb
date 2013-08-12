38906accf6173cc0295497394f6082c29b611cbc
mono <mono@monoweb.info>
2013-08-12 16:38:49 +0900

    データベースへの書き込み時にトランザクションを利用するよう変更

--- lib/purolandgreeting/database.rb
+++ lib/purolandgreeting/database.rb
@@ -8,21 +8,23 @@ module PurolandGreeting
     end
 
     def self.register(items)
-      normalizer = Normalizer.new
-      items.each do |item|
-        character = Character.where(name: normalizer.character(item[:character])).first_or_create
-        place = Place.where(name: normalizer.place(item[:place])).first_or_create
-        schedule = Schedule.where(date: item[:start_at].to_date).first_or_create
-        greeting = Greeting.where(
-          start_at: item[:start_at],
-          end_at: item[:end_at],
-          place_id: place.id,
-          schedule_id: schedule.id,
-          raw_place_name: item[:place]).first_or_create
-        appearance = Appearance.where(
-          character_id: character.id,
-          greeting_id: greeting.id,
-          raw_character_name: item[:character]).first_or_create
+      ActiveRecord::Base.transaction do
+        normalizer = Normalizer.new
+        items.each do |item|
+          character = Character.where(name: normalizer.character(item[:character])).first_or_create
+          place = Place.where(name: normalizer.place(item[:place])).first_or_create
+          schedule = Schedule.where(date: item[:start_at].to_date).first_or_create
+          greeting = Greeting.where(
+            start_at: item[:start_at],
+            end_at: item[:end_at],
+            place_id: place.id,
+            schedule_id: schedule.id,
+            raw_place_name: item[:place]).first_or_create
+          appearance = Appearance.where(
+            character_id: character.id,
+            greeting_id: greeting.id,
+            raw_character_name: item[:character]).first_or_create
+        end
       end
     end
 
@@ -42,18 +44,20 @@ module PurolandGreeting
     end
 
     def self.normalize
-      normalizer = Normalizer.new
-      normalizer.character_table.each do |before, after|
-        before_character = Character.find_by_name(before) or next
-        after_character = Character.where(name: after).first_or_create
-        Appearance.where('character_id = ?', before_character.id).update_all character_id: after_character.id
-        before_character.destroy
-      end
-      normalizer.place_table.each do |before, after|
-        before_place = Place.find_by_name(before) or next
-        after_place = Place.where(name: after).first_or_create
-        Greeting.where('place_id = ?', before_place.id).update_all character_id: after_place.id
-        before_place.destroy
+      ActiveRecord::Base.transaction do
+        normalizer = Normalizer.new
+        normalizer.character_table.each do |before, after|
+          before_character = Character.find_by_name(before) or next
+          after_character = Character.where(name: after).first_or_create
+          Appearance.where('character_id = ?', before_character.id).update_all character_id: after_character.id
+          before_character.destroy
+        end
+        normalizer.place_table.each do |before, after|
+          before_place = Place.find_by_name(before) or next
+          after_place = Place.where(name: after).first_or_create
+          Greeting.where('place_id = ?', before_place.id).update_all character_id: after_place.id
+          before_place.destroy
+        end
       end
     end
   end
