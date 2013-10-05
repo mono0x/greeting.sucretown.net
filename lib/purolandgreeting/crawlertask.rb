56ea051769b134c6fe6c3909250fbc0ead104d1a
mono <mono@monoweb.info>
2013-10-05 11:06:16 +0900

    グリーティングの変更に対応

--- lib/purolandgreeting/crawlertask.rb
+++ lib/purolandgreeting/crawlertask.rb
@@ -18,39 +18,57 @@ module PurolandGreeting
 
       normalizer = Normalizer.new
 
+      today = Date.today
+      uri = "#{ENV['ROOT_URI']}#{today.strftime('/schedule/%Y/%m/%d/')}"
+
       if registered
-        today = Date.today
+        twitter.update "#{today.strftime('%Y/%m/%d')} の予定が公開されました。 #{uri}"
+
         characters = added_items.map {|item| normalizer.character item[:character] }.uniq.sort
 
-        uri = "#{ENV['ROOT_URI']}#{today.strftime('/schedule/%Y/%m/%d/')}"
-        header = "#{today.strftime('%Y/%m/%d')} の登場キャラクター "
-        footer = " #{uri}"
-        separator = ' '
+        header = "#{today.strftime('%Y/%m/%d')} の登場キャラクター"
+        self.update_characters twitter, characters, header
+      else
+        unless added_items.empty? && deleted_items.empty?
+          twitter.update "#{today.strftime('%Y/%m/%d')} の予定が変更されました。 #{uri}"
 
-        header_size = header.size + 8 # ' (n/m): '
-        separator_size = separator.size
-        footer_size = 1 + twitter.short_url_length
+          unless added_items.empty?
+            characters = added_items.map {|item| normalizer.character item[:character] }.uniq.sort
+            header = "#{today.strftime('%Y/%m/%d')} の追加対象キャラクター"
+            self.update_characters twitter, characters, header
+          end
 
-        groups = []
-        group = []
-        characters.each do |character|
-          if header_size + footer_size + group.map(&:size).inject(0, &:+) + group.size * separator_size + character.size > 140
-            groups << group
-            group = []
-          else
-            group << character
+          unless deleted_items.empty?
+            characters = deleted_items.map {|item| normalizer.character item[:character] }.uniq.sort
+            header = "#{today.strftime('%Y/%m/%d')} の中止対象キャラクター"
+            self.update_characters twitter, characters, header
           end
         end
-        groups << group unless group.empty?
-
-        groups.each_with_index do |group, i|
-          twitter.update "#{header}(#{i + 1}/#{groups.size}): #{group.join(separator)}#{footer}"
-        end
-
       end
-
     end
 
+    def self.update_characters(twitter, characters, header, footer = nil)
+      separator = ' '
+      separator_size = separator.size
 
+      header_size = header.size + 8 # "#{header} (n/m): "
+      footer_size = footer ? footer.size + 1 : 0 # " #{footer}"
+
+      groups = []
+      group = []
+      characters.each do |character|
+        if header_size + footer_size + group.map(&:size).inject(0, &:+) + group.size * separator_size + character.size > 140
+          groups << group
+          group = []
+        else
+          group << character
+        end
+      end
+      groups << group unless group.empty?
+
+      groups.each_with_index do |group, i|
+        twitter.update [ header, "(#{i + 1}/#{groups.size}): #{group.join(separator)}", footer ].compact.join(' ')
+      end
+    end
   end
 end
