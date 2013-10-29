bfbc3850f4e351850232031cf4014e434607b0af
mono <mono@monoweb.info>
2013-10-29 12:49:24 +0900

    エラー時のリトライ処理を追加

--- lib/purolandgreeting/fetcher.rb
+++ lib/purolandgreeting/fetcher.rb
@@ -7,7 +7,9 @@ module PurolandGreeting
       agent = Mechanize.new
       agent.user_agent = 'iPhone (Ruby; http://greeting.sucretown.net/)'
 
-      index_page = agent.get('http://www.puroland.co.jp/chara_gre/')
+      index_page = self.try_request {|i|
+        agent.get('http://www.puroland.co.jp/chara_gre/')
+      }
       #index_page = agent.get('http://www.puroland.co.jp/chara_gre/?para=20130627')
       return [] if index_page.forms.empty?
 
@@ -17,11 +19,17 @@ module PurolandGreeting
         month = Integer(m[:month])
         day = Integer(m[:day])
 
-        menu_page = agent.submit(index_page.forms.first)
+        menu_page = self.try_request {|i|
+          agent.submit(index_page.forms.first)
+        }
 
         result = []
         menu_page.links_with(:href => /^chara_sche\.asp\?/).each do |link|
-          schedule_page = agent.click(link)
+          sleep 10
+
+          schedule_page = self.try_request {|i|
+            agent.click(link)
+          }
           character = link.text
           schedule_page.search('p[align="left"] font[size="-1"]').each do |font|
             t = self.normalize_date(font.text)
@@ -39,6 +47,17 @@ module PurolandGreeting
 
     def self.normalize_date(s)
       NKF.nkf '-W1 -Ww -m0Z1', s
+    end
+
+    def self.try_request(n = 10, &block)
+      n.times do |i|
+        begin
+          return block.call(i)
+        rescue
+          sleep [ 2 ** i, 30 ].min
+        end
+      end
+      raise $!
     end
   end
 end
