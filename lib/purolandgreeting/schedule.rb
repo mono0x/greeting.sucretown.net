8b074ea7fad3644f7c9933a4fb78f7914e8b2a72
mono <mono@monoweb.info>
2013-11-23 23:48:17 +0900

    scopeを利用するよう変更

--- lib/purolandgreeting/schedule.rb
+++ lib/purolandgreeting/schedule.rb
@@ -3,11 +3,11 @@ class PurolandGreeting::Schedule < ActiveRecord::Base
 
   has_many :greetings, class_name: 'PurolandGreeting::Greeting'
 
+  scope :by_month, lambda {|month|
+    where('date >= ? AND date < ?', month, month >> 1)
+  }
+
   def self.months
     select("DATE_TRUNC('month', date) AS month").group("DATE_TRUNC('month', date)").order('month DESC').map { |c| Date.parse(c.month) }
-  end
-
-  def self.by_month(month)
-    where('date >= ? AND date < ?', month, month >> 1)
   end
 end
