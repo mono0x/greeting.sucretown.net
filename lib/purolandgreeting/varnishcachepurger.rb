c5fbbc6dbc202bf9e2bfa61f050fd71bfa864941
mono <mono@monoweb.info>
2014-12-08 10:02:56 +0900

    必要がない場合にキャッシュをpurgeしないよう変更

--- lib/purolandgreeting/varnishcachepurger.rb
+++ lib/purolandgreeting/varnishcachepurger.rb
@@ -5,7 +5,9 @@ module PurolandGreeting
     def run(today = nil, now = nil, registered = nil, diff = nil)
       uri = URI.parse(ENV['VARNISH_URL'])      
       varnish = Varnish::Client.new(uri.host, uri.port, ENV['ROOT_URI'])
-      varnish.purge '.*'
+      if !diff || !diff.empty?
+        varnish.purge '.*'
+      end
     rescue
       STDERR.puts $!
     end
