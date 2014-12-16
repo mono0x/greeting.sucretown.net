fd7c4f5068a9ccde23f61d00c96e12044ae10a5e
mono <mono@monoweb.info>
2014-12-16 11:43:22 +0900

    スマートフォン向けページに対応

--- lib/purolandgreeting/fetcher.rb
+++ lib/purolandgreeting/fetcher.rb
@@ -1,9 +1,10 @@
+require 'date'
 require 'mechanize'
 require 'nkf'
 
 module PurolandGreeting
   class Fetcher
-    BASE_URI = 'http://www.puroland.co.jp/chara_gre/mobile/'
+    BASE_URI = 'http://www.puroland.co.jp/chara_gre/'
 
     def self.fetch
       agent = Mechanize.new
@@ -20,41 +21,46 @@ module PurolandGreeting
       #index_page = agent.get('http://www.puroland.co.jp/chara_gre/?para=20130627')
       return [] if index_page.forms.empty?
 
-      t = self.normalize_date(index_page.search('p[align="center"] font[size="-1"]').first.text)
-      t.match(/^\s*(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日\([日月火水木金土]\)\s*$/) do |m|
-        year = Integer(m[:year])
-        month = Integer(m[:month])
-        day = Integer(m[:day])
+      date = self.parse_date(index_page) or return []
 
-        menu_page = self.try_request {|i|
-          agent.submit(index_page.forms.first)
+      menu_page = self.try_request {|i|
+        agent.submit(index_page.forms.first)
+      }
+
+      result = []
+      menu_page.search('form[action="chara_sche.asp"]').each do |form|
+        sleep 10
+
+        tchk = form.search('input[name="TCHK"]').first['value']
+        c_key = form.search('input[name="C_KEY"]').first['value']
+        schedule_page = self.try_request {|i|
+          agent.get("#{BASE_URI}chara_sche.asp?TCHK=#{tchk}&C_KEY=#{c_key}")
         }
-
-        result = []
-        menu_page.links_with(:href => /^chara_sche\.asp\?/).each do |link|
-          sleep 10
-
-          schedule_page = self.try_request {|i|
-            agent.click(link)
-          }
-          character = link.text
-          schedule_page.search('p[align="left"] font[size="-1"]').each do |font|
-            next unless font.children.size == 3
-            time, br, place = font.children
-            next unless br.name == 'br'
-            self.normalize_date(time.text).match(/\A\s*(?<start_hour>\d+):(?<start_minute>\d+)-(?<end_hour>\d+):(?<end_minute>\d+)\z/) do |m|
-              start_at = Time.local(year, month, day, Integer(m[:start_hour]), Integer(m[:start_minute]))
-              end_at = Time.local(year, month, day, Integer(m[:end_hour]), Integer(m[:end_minute]))
-              result << { character: character, place: self.normalize_date(place.text), start_at: start_at, end_at: end_at }
-            end
+        character = schedule_page.search('#date3').first.text
+        schedule_page.search('#date').each do |div|
+          next unless div.children.size >= 3
+          time, br, place = div.children
+          next unless br.name == 'br'
+          self.normalize_string(time.text).match(/\A\s*(?<start_hour>\d+):(?<start_minute>\d+)-(?<end_hour>\d+):(?<end_minute>\d+)\z/) do |m|
+            start_at = Time.local(date.year, date.month, date.day, Integer(m[:start_hour]), Integer(m[:start_minute]))
+            end_at = Time.local(date.year, date.month, date.day, Integer(m[:end_hour]), Integer(m[:end_minute]))
+            result << { character: character, place: self.normalize_string(place.text), start_at: start_at, end_at: end_at }
           end
-          agent.back
         end
-        result
+        agent.back
       end
+      result
     end
 
-    def self.normalize_date(s)
+    def self.parse_date(index_page)
+      t = self.normalize_string(index_page.search('#date').first.text)
+      t.match(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日\([日月火水木金土]\)\s*/) do |m|
+        return Date.new(Integer(m[:year]), Integer(m[:month]), Integer(m[:day]))
+      end
+      nil
+    end
+
+    def self.normalize_string(s)
       NKF.nkf '-W1 -Ww -m0Z1', s
     end
 
