89931991137027b89881b9fa45355e878277ca92
mono <mono@monoweb.info>
2015-01-12 00:20:37 +0900

    Fetcherのテストを追加

---
+++ test/test_fetcher.rb
@@ -0,0 +1,17 @@
+require_relative 'helper'
+
+class TestFetcher < Test::Unit::TestCase
+  def test_fetch
+    VCR.use_cassette('test_fetch') do
+      result = PurolandGreeting::Fetcher.fetch(false)
+      assert_equal result.size, 81
+    end
+  end
+
+  def test_fetch_when_unpublished
+    VCR.use_cassette('test_fetch_when_unpublished') do
+      result = PurolandGreeting::Fetcher.fetch(false)
+      assert_equal result.size, 0
+    end
+  end
+end
