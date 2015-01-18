a5d118ea13c9a3cb1b5e26b3695ae16b66502627
mono <mono@monoweb.info>
2015-01-18 11:10:02 +0900

    グリーティングをフロアと場所名でソートするよう変更

--- lib/purolandgreeting/place.rb
+++ lib/purolandgreeting/place.rb
@@ -1,3 +1,12 @@
 class PurolandGreeting::Place < ActiveRecord::Base
   has_many :greetings, class_name: 'PurolandGreeting::Greeting', dependent: :delete_all
+
+  def name_without_floor
+    name.match(/\A(.+)\((?:\dF|他)\)\z/).to_a[1] || name
+  end
+
+  def floor
+    name.match(/\((\dF|他)\)/).to_a[1]
+  end
+
 end
