4e56b21bb7a7ced76c5b5011795a5afbcd26c851
mono <mono0x@users.noreply.github.com>
2015-08-30 22:39:39 +0900

    Add test cases

--- test/test_schedule.rb
+++ test/test_schedule.rb
@@ -21,4 +21,24 @@ class TestSchedule < Test::Unit::TestCase
     assert_equal months[1].month, 3
   end
 
+  def test_count_dates
+    date_from = Date.new(2015, 1, 1)
+    assert_equal PurolandGreeting::Schedule.count_dates(date_from), 3
+  end
+
+  def test_next_schedule
+    schedule = PurolandGreeting::Schedule.where('date = ?', Date.new(2015, 3, 6)).first
+    assert_equal schedule.next_schedule.date, Date.new(2015, 3, 7)
+
+    schedule = PurolandGreeting::Schedule.where('date = ?', Date.new(2015, 5, 5)).first
+    assert_equal schedule.next_schedule, nil
+  end
+
+  def test_prev_schedule
+    schedule = PurolandGreeting::Schedule.where('date = ?', Date.new(2015, 3, 6)).first
+    assert_equal schedule.prev_schedule, nil
+
+    schedule = PurolandGreeting::Schedule.where('date = ?', Date.new(2015, 5, 5)).first
+    assert_equal schedule.prev_schedule.date, Date.new(2015, 3, 7)
+  end
 end
