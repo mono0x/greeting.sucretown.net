9664ac2caff1f2a64bae0203303da22f0735a655
mono <mono0x@users.noreply.github.com>
2016-01-27 00:11:17 +0900

    Add test cases

--- test/test_live_show_fetcher.rb
+++ test/test_live_show_fetcher.rb
@@ -5,6 +5,11 @@ class TestLiveShowFetcher < Test::Unit::TestCase
     VCR.use_cassette('test_live_show_fetch') do
       items = PurolandGreeting::LiveShowFetcher.fetch
       assert_equal items.size, 12
+      assert_equal items[0][:title], 'ちっちゃな英雄(ヒーロー)'
+      assert_equal items[0][:start_at], Time.new(2016, 1, 27, 10, 25)
+      assert_equal items[0][:uri], 'http://www.puroland.jp/liveshow/little-hero/'
+      assert_equal items[0][:place], 'フェアリーランドシアター'
+      assert_equal items[0][:time], 40
     end
   end
 end
